9bfff615d64c6f5d4c0ddf957260024bb0d9a82d
mattipallyswami <mattipallyswami222@gmail.com>
2025-06-23 22:44:35 +0530

    changes

--- src/main/java/edutech/backend/EduTechBackendApplication.java
+++ src/main/java/edutech/backend/EduTechBackendApplication.java
@@ -4,9 +4,11 @@ import edutech.backend.config.JwtConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableConfigurationProperties(JwtConfig.class)
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EduTechBackendApplication {
 
 	public static void main(String[] args) {
