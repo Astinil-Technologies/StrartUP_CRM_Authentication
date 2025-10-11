8387ea6ef90e7fbbdd42aed24a18c7348df101a3
mattipallyswami <mattipallyswami222@gmail.com>
2025-10-11 11:08:17 +0530

    changes

--- src/main/java/edutech/backend/dto/SignupRequest.java
+++ src/main/java/edutech/backend/dto/SignupRequest.java
@@ -9,13 +9,14 @@ import jakarta.validation.constraints.NotEmpty;
 @Data
 @Builder
 public class SignupRequest {
-    @NotNull(message = "FirstName is required")
-    @NotEmpty(message = "FirstName cannot be empty")
-    private String firstName;
-    @NotNull(message = "LastName is required")
-    @NotEmpty(message = "LastName cannot be empty")
-    private String lastName;
-    @NotNull(message = "Username is required")
+//    @NotNull(message = "FirstName is required")
+//    @NotEmpty(message = "FirstName cannot be empty")
+//    private String firstName;
+//    @NotNull(message = "LastName is required")
+//    @NotEmpty(message = "LastName cannot be empty")
+//    private String lastName;
+
+   @NotNull(message = "Username is required")
     @NotEmpty(message = "Username cannot be empty")
     private String username;
     @NotNull(message = "Email is required")
