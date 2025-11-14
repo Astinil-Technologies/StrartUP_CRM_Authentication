8b6c3eb9060d4a0f7533760d3d154743994de442
Mrakesh567 <rakimaradana2727@gmail.com>
2025-11-14 15:11:45 +0530

    Role based changes on Timesheet

--- src/main/java/edutech/backend/config/WebSecurityConfig.java
+++ src/main/java/edutech/backend/config/WebSecurityConfig.java
@@ -43,7 +43,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-              //  .cors(Customizer.withDefaults())
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // ✅ this line
